6b2b3ff3edbb671cd95c056e65c199a51cc77b95
jamenendez88 <josearmandomenendezclavijo@gmail.com>
2018-11-28 16:04:11 -0800

    probando cors

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ app.set('port', process.env.PORT || 3000);
 //Middlewares
 app.use(morgan('dev'));
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: '*' }));
 
 //Routes
 //Robot actions
